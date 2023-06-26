ee282c66872da2b35be4fc113d3f94b96d28f100
Guy <guyshilon30@gmail.com>
2023-06-26 15:30:46 +0300

    apps-icon

--- apps/keep/cmps/NoteHeader.js
+++ apps/keep/cmps/NoteHeader.js
@@ -1,5 +1,6 @@
 import { svgService } from '../../../services/svg-service.js'
 import NoteFilter from './NoteFilter.js'
+import AppsModal from '../../../cmps/AppsModal.js'
 
 export default {
   template: `
@@ -13,10 +14,17 @@ export default {
            <NoteFilter @filter="setFilterBy" />
             </div>
 
-            <router-link to="/mail"><img class="logo" src='././assets/style/apps/mail/imgs/mail.png' alt="" /></router-link>
+           <img @click="openModal" class="apps-icon" src="../assets/img/apps-icon.png" alt="" />
+           <AppsModal v-show="showModal" />
             
           </header>
       `,
+
+  data() {
+    return {
+      showModal: false,
+    }
+  },
 
   methods: {
     getSvg(iconName) {
@@ -26,9 +34,13 @@ export default {
     setFilterBy(filterBy) {
       this.$emit('setFilter', filterBy)
     },
+    openModal() {
+      this.showModal = !this.showModal
+    },
   },
 
   components: {
     NoteFilter,
+    AppsModal,
   },
 }
